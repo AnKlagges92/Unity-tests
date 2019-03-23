3a33178bf73f14990edd0ef7e179ce701cebdf1e
Andres <anklagges@hotmail.com>
2019-03-23 13:24:28 -0300

    Several minor fixes.

--- Assets/02_Scripts/UIArchitecture/NEW/LevelUIController.cs
+++ Assets/02_Scripts/UIArchitecture/NEW/LevelUIController.cs
@@ -29,7 +29,7 @@ namespace UI
             }
             else
             {
-                SetText(level + "-" + maxLevel);
+                SetText(level + " /" + maxLevel);
             }
         }
     }
